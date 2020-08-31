2b8e636105a3f7c931d4e9fbd939bc1fcd184411
touchguy <touchguy70@gmail.com>
2020-08-31 12:01:26 +0900

    test입니다.

--- app/src/main/java/kr/co/munjanara/examokhttp/MainActivity.java
+++ app/src/main/java/kr/co/munjanara/examokhttp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //  change.. 11
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
