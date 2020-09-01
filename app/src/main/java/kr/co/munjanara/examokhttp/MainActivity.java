c1ce86267229173ce792792f4f89079696d0b193
touchguy <touchguy70@gmail.com>
2020-09-01 12:30:08 +0900

    test입니다.

--- app/src/main/java/kr/co/munjanara/examokhttp/MainActivity.java
+++ app/src/main/java/kr/co/munjanara/examokhttp/MainActivity.java
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import android.icu.util.LocaleData;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.ResultReceiver;
@@ -28,23 +29,43 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         new HttpAsyncTask().execute( "https://goo.gl/eIXu9l");
     }
 
     private static class HttpAsyncTask extends AsyncTask<String, Void, String> {
         OkHttpClient client = new OkHttpClient();
+        List<Weather> weatherList = new ArrayList<>();
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
             if(s != null) {
-                Log.d(TAG, s);
+//                Log.d(TAG, s);
+                JSONArray jsonArray = null;
+                try {
+                    jsonArray = new JSONArray(s);
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String country = jsonObject.getString("country");
+                        String weather = jsonObject.getString("weather");
+                        String temperature = jsonObject.getString("temperature");
+                        Weather w = new Weather(country, weather, temperature);
+                        weatherList.add(w);
+                    }
+
+                    Log.d(TAG, weatherList.toString());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
@@ -52,30 +73,37 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(String... strings) {
             String result = null;
             String strUrl = strings[0];
-            List<Weather> weatherList = new ArrayList<>();
+
+
+//            List<String> tmp = new ArrayList<>();
 
             try {
                 Request request = new Request.Builder()
                         .url(strUrl)
                         .build();
                 Response response = client.newCall(request).execute();
-
-                Gson    gson = new Gson();
-
-                Type listType = new TypeToken<ArrayList<Weather>>() {}.getType();
-                weatherList = gson.fromJson( response.body().string(), listType);
-
-//                JSONArray jsonArray = new JSONArray(response.body().string());
-//                for (int i = 0; i < jsonArray.length(); i++) {
-//                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-//                    String country = jsonObject.getString("country");
-//                    String weather = jsonObject.getString("weather");
-//                    String temperature = jsonObject.getString("temperature");
-//                    Weather w = new Weather(country, weather, temperature);
-//                    weatherList.add(w);
-//                }
-                Log.d(TAG, "doinbackground" + weatherList.toString() );
+                result = response.body().string();
+/*
+                JSONArray jsonArray = new JSONArray(response.body().string());
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String country = jsonObject.getString("country");
+                    String weather = jsonObject.getString("weather");
+                    String temperature = jsonObject.getString("temperature");
+                    Weather w = new Weather(country, weather, temperature);
+                    weatherList.add(w);
+                }
 //                Log.d(TAG, "doinbackground" + weatherList.toString() );
+
+                for (int i = 0; i < weatherList.size(); i++) {
+                    Log.d(TAG, "CONTRY = " + weatherList.get(i).getCountry().toString());
+                    Log.d(TAG, "WEATHER = " + weatherList.get(i).getWeather().toString());
+                    Log.d(TAG, "TEMPERATURE = " + weatherList.get(i).getTemperature().toString());
+
+                }
+                */
+
+
 
             } catch (IOException e) {
                 e.printStackTrace();;
